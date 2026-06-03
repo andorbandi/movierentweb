fcaf2f88e5e35980dd0d407c6138d2421df5f700
Szép Andor <andorbandi08@gmail.com>
2026-06-03 15:48:02 +0200

    modalablak hozzáadva

--- src/movies.js
+++ src/movies.js
@@ -4,10 +4,81 @@ import './style.css'
 
 import { getMovies, createMovie } from './apiService.js'
 
+const doc = {
+    modalbox: document.querySelector('#modalbox'),
+    modal: document.querySelector('.modal'),
+    modalBackdrop: document.querySelector('#modal-backdrop'),
+    addButton: document.querySelector('#addButton'),
+    closeButton: document.querySelectorAll('.closeButton'),
+    movieform: document.querySelector('#movieForm')
+}
+
+const modalState = {
+    _show: false,
+    get show() {
+        return this._show
+    },
+    set show(value) {
+        this._show = value
+        //renderelés
+        renderModal()
+    }
+}
+
+doc.addButton.addEventListener('click', () => {
+    startAddMovie()
+    console.log('Hozzáadás gombra kattintva');
+})
+
+for(let i = 0; i < 2; i++) {
+    doc.closeButton[i].addEventListener('click', () => {
+        console.log('Bezár gombra kattintva');
+        modalState.show = false
+    })
+}
+
+doc.movieform.addEventListener('submit', (event) => {
+    e.preventDefault()
+    console.log('űrlap feldolgozása');
+    const formData = new FormData(e.target)
+    const title = formData.get('title')
+    console.log(title);
+})
+
+
+doc.closeButton.addEventListener('click', () => {
+    console.log('Bezár gombra kattintva');
+    modalState.show = false
+})
+
+function startAddMovie() {
+    modalState.show = true
+}
+function renderModal() {
+    if (modalState.show) {
+        doc.modalbox.classList.remove('d-none')
+        doc.modal.classList.add('show')
+        doc.modal.classList.add('d-block')
+        doc.modalBackdrop.classList.add('d-block')
+        setTimeout(() => {
+            doc.modalBackdrop.classList.add('show')
+        }, 10)
+    } else {
+        doc.modal.classList.remove('show')
+        doc.modalBackdrop.classList.remove('show')
+        setTimeout(() => {
+            doc.modal.classList.remove('d-block')
+            doc.modalBackdrop.classList.remove('d-block')
+            doc.modalbox.classList.add('d-none')
+        }, 150)
+    }
+}
+
 function startGetMovies() {
     getMovies().then(res => {
         console.log(res);
     })
 }
 
+renderModal()
 startGetMovies()
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 export default {
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        movies: 'movies.html'
+      }
+    }
   }
 }
